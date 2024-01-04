aa19ceae666c263f4bd3a19bb0f3ef03b8320e81
Alexandre Uglar <acuglar@gmail.com>
2024-01-04 15:16:59 -0300

    Implementa regras  em conformidade com os requisitos do projeto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,17 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        private string modelo;
+        private string imei;
+        private int memoria;
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.modelo = modelo;
+            this.imei = imei;
+            this.memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "n1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine();
+
+Console.WriteLine("Smartphone IPhone: ");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "x", imei: "222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+Console.WriteLine();
